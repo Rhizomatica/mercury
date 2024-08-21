fb647d091bbdaed4915c9a0f94d4d555bc8fa1d3
Rafael Diniz <rafael@riseup.net>
2024-08-21 03:10:48 +0100

    more improvements towards multi-platform port

--- source/audioio/ffaudio/ffaudio/wasapi.c
+++ source/audioio/ffaudio/ffaudio/wasapi.c
@@ -285,6 +285,8 @@ static void wf_from_ff(WAVEFORMATEX *wf, const ffaudio_conf *conf)
 	switch (conf->format) {
 	case FFAUDIO_F_FLOAT32:
 		wf->wFormatTag = 3;
+	case FFAUDIO_F_FLOAT64:
+		wf->wFormatTag = 3;
 		break;
 	}
 
@@ -447,6 +449,7 @@ static int wasapi_find_fmt(IAudioClient *client, IMMDevice *dev, ffaudio_conf *c
 		return 0;
 
 	static const ffushort fmts[] = {
+		FFAUDIO_F_FLOAT64,
 		FFAUDIO_F_FLOAT32,
 		FFAUDIO_F_INT32,
 		FFAUDIO_F_INT24,
