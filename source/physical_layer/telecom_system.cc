9b94270a5cd8aaf19726893e7fa3ec27114e9d26
Rafael Diniz <rafael@riseup.net>
2024-08-30 21:58:58 +0100

    code works!

--- source/physical_layer/telecom_system.cc
+++ source/physical_layer/telecom_system.cc
@@ -1100,8 +1100,6 @@ void cl_telecom_system::RX_TEST_process_main()
                data_container.buffer_Nsymb,
                data_container.Nofdm * data_container.interpolation_rate);
 
-	printf("location_of_last_frame: %d\n", location_of_last_frame);
-
 	rx_transfer(&data_container.passband_delayed_data[location_of_last_frame], data_container.Nofdm * data_container.interpolation_rate);
 
 	data_container.frames_to_read--;
@@ -1112,7 +1110,6 @@ void cl_telecom_system::RX_TEST_process_main()
 
 	if(data_container.frames_to_read == 0)
 	{
-		printf("chunk size: %d\n", data_container.Nofdm * data_container.buffer_Nsymb * data_container.interpolation_rate);
 		for(int i=0; i < data_container.Nofdm * data_container.buffer_Nsymb * data_container.interpolation_rate; i++)
 		{
 			data_container.ready_to_process_passband_delayed_data[i] = data_container.passband_delayed_data[i];
